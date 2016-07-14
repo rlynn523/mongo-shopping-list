3c0984eac94b76b59c5820f542e5407e70dddbf2
Ryan Lynn <ryanlynn523@gmail.com>
2016-07-14 10:09:13 -0400

    GET and POST methods pass mocha

--- config.js
+++ config.js
@@ -1,9 +1,6 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
-                       global.DATABASE_URL;
-
-
-                       /* ||
+                       global.DATABASE_URL ||
                        process.env.NODE_ENV === 'production' ?
                             'mongodb://localhost/shopping-list' :
-                            'mongodb://localhost/shopping-list-dev';*/
+                            'mongodb://localhost/shopping-list-dev';
 exports.PORT = process.env.PORT || 8080;
