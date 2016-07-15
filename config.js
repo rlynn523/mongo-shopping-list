65262fb971c94bac6b85c4bfabe1305add75b380
Ryan Lynn <ryanlynn523@gmail.com>
2016-07-14 20:49:40 -0400

    fixed the test url config

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
-exports.DATABASE_URL = process.env.DATABASE_URL ||
-    global.DATABASE_URL ||
-    process.env.NODE_ENV === 'production' ?
+var db = process.env.NODE_ENV === 'production' ?
     'mongodb://ryanlynn:rango123@ds049150.mlab.com:49150/heroku_r4flkwdr' :
     'mongodb://localhost/shopping-list-dev';
+exports.DATABASE_URL = process.env.DATABASE_URL ||
+    global.DATABASE_URL || db;
 exports.PORT = process.env.PORT || 8080;
