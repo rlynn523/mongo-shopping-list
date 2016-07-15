40152474e062f8093577ad7ff6614886c0a614fc
Ryan Lynn <ryanlynn523@gmail.com>
2016-07-14 20:10:24 -0400

    added mLab url

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
-                       global.DATABASE_URL ||
-                       process.env.NODE_ENV === 'production' ?
-                            'mongodb://localhost/shopping-list' :
-                            'mongodb://localhost/shopping-list-dev';
+    global.DATABASE_URL ||
+    process.env.NODE_ENV === 'production' ?
+    'mongodb://<ryanlynn>:<rango123>@ds049150.mlab.com:49150/heroku_r4flkwdr' :
+    'mongodb://localhost/shopping-list-dev';
 exports.PORT = process.env.PORT || 8080;
